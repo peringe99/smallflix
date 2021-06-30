cf84474bbb256238e28196069306faf961dfa0ab
Gabriel Holmkvist <gabbeholman99@gmail.com>
2021-06-30 13:17:03 +0200

    random

---
+++ index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const express = require('express')
+
+await mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
