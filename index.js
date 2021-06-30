550fa5c446a9dbbcf8090e334fbdc527394da8d2
Gabriel Holmkvist <gabbeholman99@gmail.com>
2021-06-30 13:23:06 +0200

    app listen added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express')
+var app = express();
+
+app.get('/', function (req ,res) {
+    res.send('Hello World');
+});
+
+app.listen(8000, function () {
+    console.log('Listening to port 8000');
+})
 
 await mongoose.connect('mongodb://localhost/my_database', {
   useNewUrlParser: true,
